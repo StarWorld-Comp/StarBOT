a295f87ead73f7c92edf6f85ba33fb598a2f5b4c
ShidoKageno <levkovichartiom78@gmail.com>
2024-08-12 12:30:14 +0500

    Добавлено события недоступности сервера

--- events/onGuildUnavailable.js
+++ events/onGuildUnavailable.js
@@ -2,5 +2,8 @@ module.exports = [{
   name: "onGuildUnavailable",
   type: "guildUnavailable",
   channel: "$getGuildVar[logs;$guildID]",
-  code: ``
+  code: `$description[Текущая гильдия стала недоступна]
+$addField[Возможные причины;* Сбой сервера\n* Закрытие гильдии;false]
+$footer[Id гильдии#COLON# $guildID;$guildIcon]
+$addTimestamp`
 }];
