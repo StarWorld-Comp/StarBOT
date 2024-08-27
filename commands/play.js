8b90a83bf36704c6ad6395dd80092a794146a13c
ShidoKageno <levkovichartiom78@gmail.com>
2024-08-27 08:52:03 +0500

    Update /play

--- commands/play.js
+++ commands/play.js
@@ -6,19 +6,23 @@ module.exports = [{
   code: `
 $if[$isAutocomplete==true]
 $autoCompleteRespond[$nonEscape[$get[query]]]
+$if[$isValidLink[$slashOption[query]]==true]
+$let[query;$slashOption[query]#SEMI#$slashOption[query]
+$endif
 $if[$slashOption[query]==]
-$let[query;укажите название трека или url.#SEMI#track or url]
+$let[query;укажите название трека или url.#SEMI#track-or-url]
 $else
 $let[query;$search[$slashOption[query];youtube;{title}#SEMI#{title};8;#SEMI#]]
 $endif
-$suppressErrors
 $else
-$playTrack[$slashOption[query];youtube]
+$playTrack[$nonEscape[$slashOption[query]];youtube]
 $if[$hasPlayer==false]
-$joinVC[$voiceID[$authorID]]
+$joinVC[$voiceID[$authorID];true;false;true;default]
 $endif
-$interactionFollowUp[<:music:1264802104925749379> Трек **$slashOption[query]** - **$username[$authorID]** добавлен в очередь.]
-$interactionDefer[true]
+$interactionEdit[<:music:1264885613497159782> Трек **$slashOption[query]** — **$username[$authorID]** добавлен в очередь.]
+$wait[1s]
+$interactionReply[Загрузка трека...]
+$suppressErrors
 
 $onlyIf[$voiceID[$authorID]!=;{newEmbed:{color:#f1090b}{description:Присоединитесь к любому голосовому каналу.}{author:Ошибка:$get[error.icon]}{timestamp}}{ephemeral}{interaction}]
 $let[error.icon;https://cdn.discordapp.com/attachments/1162658570609901641/1244579676584935465/776404508515368972.png?ex=6655a0a6&is=66544f26&hm=a068d0186245402f33b93a145dc53178d854e0b9eeec437571f0110a56038c59&]
